1d3d2c45a07dced7ac072c54cce1cf532c852121
costmon <qkrrbguswkd@Naver.com>
2022-01-28 11:36:09 +0900

    day20 추가파일

---
+++ day21/fn_hello.py
@@ -0,0 +1,14 @@
+#함수의 정의와 호출
+#def 키워드를 사용
+
+def say_hello():
+    print("안녕하세요")
+
+def say_hello2(name):  #nam3 = "지하나"
+    print("안녕하세요~", name)
+
+say_hello()
+say_hello()
+
+say_hello2("지하나")
+say_hello2("지하나")
---
+++ day21/fn_test.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a+b
+
+print(add(3,4))
+
+def add(a, b):
+    result = a + b
+    return result
