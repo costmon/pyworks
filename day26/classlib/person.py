3f5189718805469a50389d780f026a412c2fc4aa
costmon <qkrrbguswkd@Naver.com>
2022-02-15 12:49:23 +0900

    day30

--- day26/classlib/person.py
+++ day26/classlib/person.py
@@ -1,7 +1,7 @@
 
 class Person:
-    def __init__(self, name, age):
-        self.name = name
+    def __init__(self, name, age):   #생성자(함수)
+        self.name = name             #멤버변수
         self.age = age
 
     def getname(self):
@@ -10,13 +10,17 @@ class Person:
     def getage(self):
         return self.age
 
-class Employee(Person): #Person 클래스를 상속 받음
+class Employee(Person): #Person 클래스를 상속 받음, 자식클래스
     pass
 
-e1 = Employee("지민", 30)
-print("이름 :", e1.getname())   #부모클래스의 멤버를 공유
-print("나이 :", e1.getage())
+if __name__ == "__main__":
+    e1 = Employee("지민", 30)
+    print("이름 :", e1.getname())   #부모클래스의 멤버를 공유
+    print("나이 :", e1.getage())
 
+    e2 = Employee("진", 28)
+    print("이름 : " + e2.getname())
+    print("나이 : " ,str(e2.getage()))
 '''
 p1 = Person('뷔', 28)
 print("이름 :", p1.getname())
