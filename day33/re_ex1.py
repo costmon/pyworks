c27825105addd11a33f686442cd8e55a2373cc7c
costmon <qkrrbguswkd@Naver.com>
2022-02-21 12:45:09 +0900

    day33 추가파일

--- day33/re_ex1.py
+++ day33/re_ex1.py
@@ -1,7 +1,10 @@
 import re
 
+# match()는 첫문자부터 일치하는 검색 ->find()와 유사함
+
+str = "before"
 pattern1 = re.compile("[a-z]+")   # 정규 표현식
-result = pattern1.match("before") # 처음 문자부터 검색
+result = pattern1.match(str) # 처음 문자부터 검색
 print(result)
 
 pattern2 = re.compile("[0-9]+\s[a-z]+")
